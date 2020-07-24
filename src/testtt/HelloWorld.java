de01dda42c1fc39fee6d3a361e7fab4781303301
sh_yp <sh_ypwang@hotmail.com>
2020-07-24 14:17:29 +0800

    onepip2 change 3

--- src/testtt/HelloWorld.java
+++ src/testtt/HelloWorld.java
@@ -3,8 +3,8 @@ package testtt;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello x");
-		System.out.println("hello");
+		System.out.println("hello x y");
+		System.out.println("hello y");
 		
 	}
 
